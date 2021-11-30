44a6f65c6a6178764e3dccfa5844aa6287957f50
Kurt McKee <contactme@kurtmckee.org>
2021-11-30 07:48:15 -0600

    Update tests for merge_keywords() helper function

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -6,54 +6,66 @@ from globus_automate_client import helpers
 @pytest.mark.parametrize(
     "args, expected, message",
     (
-        # List
-        (([],), set(), "empty list identity not maintained"),
-        (([1],), {1}, "list identity not maintained"),
-        (([1, 1],), {1}, "in-list deduplication not performed"),
-        (([1], [1]), {1}, "cross-list deduplication not performed"),
-        (([1, 2],), {1, 2}, "unique items missing"),
-        # Dictionary
-        (({}, "k"), None, "dict with no matching key must return None"),
-        (({"k": [1]},), None, "dict w/o matching key specified must return None"),
-        (({"k": [1]}, "k"), {1}, "dict w/o list and w/ matching key must be added"),
-        (([1], {"k": [2]}, "k"), {1, 2}, "dict with matching key must be added"),
-        (([1], {"k": [1]}, "k"), {1}, "dict values must be de-duplicated"),
-        (([1], {"k": 2}, "k"), {1}, "scalar value in dict must be ignored"),
+        # Null
+        ((None, {}), None, "If nothing is specified, None must be returned"),
+        #
+        # List with no dict keys
+        (([], {}), set(), "empty list identity not maintained"),
+        ((["1"], {}), {"1"}, "list identity not maintained"),
+        ((["1", "1"], {}), {"1"}, "deduplication not performed"),
+        ((["1", "2"], {}), {"1", "2"}, "unique items missing"),
+        #
+        # Dict with no list
+        ((None, {}, "-"), None, "dict without matching key must return None"),
+        ((None, {"k": ["1"]},), None, "dict with no key specified must return None"),
+        ((None, {"k": ["1"]}, "-"), None, "dict without matching key must return None"),
+        ((None, {"k": ["1"]}, "k"), {"1"}, "dict with matching key must be added"),
+        ((None, {"k": ["1", "1"]}, "k"), {"1"}, "dict values must be de-duped"),
+        (
+            (None, {"k1": ["1"], "k2": ["1"]}, "k1", "k2"),
+            {"1"},
+            "dict values must be de-duped",
+        ),
+        (
+            (None, {"k1": ["1"], "k2": ["2"]}, "k1", "k2"),
+            {"1", "2"},
+            "dict values must all be added",
+        ),
+        #
+        # Dict with scalar values
+        ((None, {"k": "1"}, "k"), {"1"}, "scalar value must be added"),
+        (
+            (None, {"k1": "1", "k2": "1"}, "k1", "k2"),
+            {"1"},
+            "scalar values must be de-duped",
+        ),
+        #
+        # List combined with a dict
+        ((["1"], {"k": ["2"]}, "-"), {"1"}, "combo w/o matching key must not be added"),
+        ((["1"], {"k": ["2"]}, "k"), {"1", "2"}, "combo w/ matching key must be added"),
+        ((["1"], {"k": ["1"]}, "k"), {"1"}, "combo values must be de-duplicated"),
+        ((["1"], {"k": "2"}, "k"), {"1", "2"}, "combo w/ scalar value must be added"),
         # Multiple values
         (
-            ([1, 2], [2, 3], {"k1": [4], "k2": [5], "k3": [6]}, "k2", "k3", "bogus"),
-            {1, 2, 3, 5, 6},
+            (["1", "2"], {"k1": "3", "k2": ["4"], "k3": "5"}, "k2", "k3", "-"),
+            {"1", "2", "4", "5"},
             "all values must be found",
         ),
     ),
 )
-def test_merge_lists(args, expected, message):
+def test_merge_keywords(args, expected, message):
     """Validate globus_automate_client.helpers.merge_lists()."""
 
-    original_lengths = sum(len(d) for d in args if isinstance(d, dict))
-    result = helpers.merge_lists(*args)
-    final_lengths = sum(len(d) for d in args if isinstance(d, dict))
+    original_key_count = len(args[1])
+    result = helpers.merge_keywords(*args)
+    final_key_count = len(args[1])
+    expected_key_count = original_key_count - (len(args) - 2)
+    if "-" in args:
+        expected_key_count += 1  # One key was bogus
     if expected is None:
         assert result is None, message
-        assert original_lengths == final_lengths, "dicts modified despite no matches"
+        assert final_key_count == original_key_count, "dict unexpectedly modified"
     else:
         assert result is not None, "*result* must not be None"
         assert set(result) == expected, message
-        if original_lengths:
-            assert original_lengths > final_lengths, "dicts not modified"
-
-
-@pytest.mark.parametrize(
-    "pop, args, expected, message",
-    (
-        (True, ({"k": [1]}, "k"), {}, "dict not modified when match found"),
-        (True, ({"k": [1]}, "x"), {"k": [1]}, "dict modified when no match found"),
-        (False, ({"k": [1]}, "k"), {"k": [1]}, "dict modified when match found"),
-        (False, ({"k": [1]}, "x"), {"k": [1]}, "dict modified when no match found"),
-    ),
-)
-def test_merge_lists_pop_behavior(args, pop, expected, message):
-    """Validate input dicts are (or are not) modified in-place."""
-
-    helpers.merge_lists(*args, pop_dict_lists=pop)
-    assert args[0] == expected, message
+        assert final_key_count == expected_key_count, "dicts not modified correctly"
